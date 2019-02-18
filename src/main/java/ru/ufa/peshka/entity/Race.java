44af18b3a5c65e735751e86650ebe177297d588d
yukidji <yukbaeva@gmail.com>
2019-02-18 21:18:24 +0500

    Создание новой ветки DAO

--- src/main/java/ru/ufa/peshka/entity/Race.java
+++ src/main/java/ru/ufa/peshka/entity/Race.java
@@ -16,7 +16,6 @@ public abstract class Race extends AbstractID {
      */
     private String timeFinish;
 
-
     //***************************************************
 
     public ClassDistance getClassDistance() {
